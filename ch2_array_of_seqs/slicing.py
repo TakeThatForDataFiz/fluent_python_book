75b2ca219066e64d7469d3b5dc8b5ed3750a7c3e
TakeThatForDataFiz <johnlunz8@gmail.com>
2023-02-18 23:45:02 -0500

    Added Slicing Examples from lizard book

---
+++ ch2_array_of_seqs/slicing.py
@@ -0,0 +1,17 @@
+# slices exclude the last item
+l = [10, 20, 30, 40, 50, 60]
+
+# print(l[:2]) # split  at 2
+# print(l[2:])
+# print(l[:3]) # split at 3
+# print(l[3:])
+
+
+# slice objects
+
+# stride variations
+s = 'bicycle'
+
+print(s[::3])
+print(s[::-1]) # reversed
+print(s[::-2])
